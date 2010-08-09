a60f828e4e16f0e68ddc246443b5190aa99b5fad
roettig <devnull@localhost>
2010-08-09 20:33:09 +0200

    minor fixes

--- src/org/roettig/PDBTools/PDBTools.java
+++ src/org/roettig/PDBTools/PDBTools.java
@@ -1,5 +1,7 @@
 package org.roettig.PDBTools;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
@@ -11,6 +13,8 @@ import org.biojava.bio.seq.impl.SimpleSequenceFactory;
 import org.biojava.bio.seq.io.*;
 import org.biojava.bio.symbol.*;
 import org.biojava.bio.structure.*;
+import org.biojava.bio.structure.io.PDBFileReader;
+import org.biojava.bio.structure.io.StructureIOFile;
 import org.roettig.SequenceTools.PairwiseAlignment;
 import org.roettig.SequenceTools.SeqTools;
 import org.biojava.bio.symbol.SimpleSymbolList;
@@ -50,6 +54,61 @@ public class PDBTools
             logger.info("did not find requested group "+rl);
         }
       return found;
+    }
+    
+    private static String getElement(Atom at)
+    {
+	if(at.getName().contains("C"))
+	    return "C";
+	if(at.getName().contains("N"))
+	    return "N";
+	if(at.getName().contains("O"))
+	    return "O";
+	if(at.getName().contains("H"))
+	    return "H";
+	if(at.getName().contains("P"))
+	    return "P";
+	if(at.getName().contains("S"))
+	    return "S";
+	return " ";
+    }
+    
+    public static void writeAtomData(PrintWriter out, List<Group> groups)
+    {
+	
+	long idx = 1;
+	
+	for(Group g: groups)
+	{
+	    String Record_ID = null;
+	    if(g instanceof AminoAcid)
+		Record_ID = "ATOM";
+	    else
+		Record_ID = "HETATM";
+	    for(Atom at: g.getAtoms())
+	    {
+		//System.out.println(String.format("%5d",idx));
+		//                                         "%5d %-4.4s%c%3.3s %c%4d%c   %8.3f%8.3f%8.3f%6.2f%6.2f      %4.4s%2.2s%2.2s"
+		out.write(String.format(Locale.ENGLISH,"%-6s%5d %-4.4s%s%3.3s %s%4d%s   %8.3f%8.3f%8.3f%6.2f%6.2f      %4.4s%2.2s%2.2s\n",
+			Record_ID,
+			idx,
+			at.getName(),
+			at.getAltLoc(),
+			g.getPDBName(),
+			g.getParent().getName(),
+			Integer.parseInt(g.getPDBCode()),
+			" ",
+			at.getX(),at.getY(),at.getZ(),1.0,0.0," ",getElement(at)," ")
+			);
+		//out.write(String.format("%-6s%5d %-4.4s %3.3s\n",Record_ID,idx,at.getName(),g.getPDBName()));
+		//out.write(String.format(FORMAT_ATOM,idx,Record_ID,idx,' ',g.getPDBName(),at.getName()," ",g.getPDBName()," ",g.getParent().getName(),g.getPDBCode(),"   ",at.getX(),at.getY(),at.getZ(),0.0,0.0," ","   "));
+		//out.write(at.getPDBline());
+		idx++;
+		//System.out.println(at.getFullName());
+	    }
+	    
+	}
+	
     }
     
     public static Vector<Group> getGroups(Structure struc, List<ResidueLocator> centerlocators)
@@ -206,6 +265,63 @@ public class PDBTools
         return ascresidues;
     }
     
+    public static List<Group> getASCGroups(Structure struc, List<ResidueLocator> centerlocators, List<Vector3> positions, double cutoff)
+    {
+        List<Group> centers     = getGroups(struc, centerlocators);
+        
+        List<Chain>    chains     = struc.getChains(0);
+        List<Group> ascresidues = new Vector<Group>();
+        
+        Chain chain1       = chains.get(0);
+        HashMap<Integer,Integer> seqIDX2pdbIDX = new HashMap<Integer,Integer>();
+        HashMap<Integer,Integer> pdbIDX2seqIDX = new HashMap<Integer,Integer>();
+        
+        Sequence seqChain1 = getSequence(chain1,seqIDX2pdbIDX,pdbIDX2seqIDX);
+        
+        PairwiseAlignment pwa = new PairwiseAlignment();
+        
+        for(Chain c: chains)
+        {
+            Sequence seqChain = getSequence(c,null,null);
+            
+            double pid = 0.0;
+            try
+            {
+              pid = pwa.align(seqChain1, seqChain);
+            }
+            catch( IllegalSymbolException e)
+            {
+                e.printStackTrace();
+            }
+            
+            if(pid==1.0)
+            {
+                Vector<String> allowed = new Vector<String>();
+                allowed.addElement("MSE");
+                
+                logger.info("examining chain "+c.getName()+" (which is identical to first chain)");
+                
+                List<Group> groups  = getGroups( c , allowed);
+                List<Group> vinc    = getNearbyGroups(groups,centers, cutoff);
+                List<Group> vinc2   = getNearbyGroups2(groups,positions, cutoff);
+                HashSet<Group> collected = new HashSet<Group>();
+                
+                for(Group g: vinc)
+                {
+                    collected.add(g);
+                    ascresidues.add(g);
+                }
+                for(Group g: vinc2)
+                {
+                    if(collected.contains(g))
+                         continue;
+                    ascresidues.add(g);
+                }                
+            }
+        }
+        return ascresidues;
+    }
+    
     public static Sequence getSequence(Chain cha, Map<Integer,Integer> seqIDX2pdbIDX, Map<Integer,Integer> pdbIDX2seqIDX)
     {        
         String seq = "";
@@ -307,5 +423,28 @@ public class PDBTools
             
         }
         return aas;
+    }
+    
+    public static void main(String args[]) throws StructureException, FileNotFoundException
+    {
+	PDBFileReader pdbreader = new PDBFileReader();
+        
+        Structure struc = null;
+        try
+        {
+         struc = pdbreader.getStructure("/tmp/pdbfile");
+        }
+        catch(Exception e)
+        {
+         
+        }
+        Group g = struc.findGroup("A","234");
+        System.out.println(g);
+        List<Group> gs = new ArrayList<Group>();
+        gs.add(g);
+        gs.add(g);
+        PrintWriter raus = new PrintWriter("/tmp/raus");
+        writeAtomData(raus,gs);
+        raus.close();
     }
 }
--- src/org/roettig/PDBTools/ResidueLocator.java
+++ src/org/roettig/PDBTools/ResidueLocator.java
@@ -4,6 +4,16 @@ import java.io.Serializable;
 
 public class ResidueLocator implements Serializable
 {
+  private static final long serialVersionUID = 7426685724689835032L;
+  
+  public static ResidueLocator fromString(String s)
+  {
+      if(s.startsWith("[")&&s.endsWith("]"))
+	  s = s.substring(1,s.length()-1);
+      String toks[] = s.split("\\|");
+      return new ResidueLocator(toks[0],toks[1],Integer.parseInt(toks[2]));
+  }
+  
   public ResidueLocator(String _chain, String _name, int _idx)
   {
      chain = _chain;
@@ -19,4 +29,10 @@ public class ResidueLocator implements Serializable
   public String chain;
   public String name;
   public int idx;
+  
+  public static void main(String args[])
+  {
+     System.out.println(ResidueLocator.fromString("[A|PHE|550]").toString());
+  }
+  
 }
